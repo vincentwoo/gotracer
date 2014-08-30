38899e54bcbcf33979251f3e5108c92b2f9c113c
Vincent Woo <me@vincentwoo.com>
2014-08-29 20:19:53 -0700

    draw a bad gradient image

--- tracer.go
+++ tracer.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"image"
 	"image/color"
 	"image/png"
@@ -10,17 +9,24 @@ import (
 )
 
 func main() {
-	img := image.NewRGBA(image.Rect(0, 0, 100, 100))
-	img.Set(5, 5, color.RGBA{255, 0, 0, 255})
+	img := renderImage(100, 100)
 
-	outfile, err := os.Create("test.png")
-	if err != nil {
-		fmt.Println("error creating file")
-		return
-	}
+	outfile, _ := os.Create("out.png")
 	defer outfile.Close()
 
 	writer := io.Writer(outfile)
 
 	png.Encode(writer, img)
+}
+
+func renderImage(width, height int) (img *image.RGBA) {
+	img = image.NewRGBA(image.Rect(0, 0, width, height))
+
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			img.Set(x, y, color.RGBA{uint8(x), uint8(y), 0, 255})
+		}
+	}
+
+	return
 }
